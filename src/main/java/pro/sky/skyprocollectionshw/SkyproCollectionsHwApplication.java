cb1ba3a0bc7590a9259716c447b99ed7f19514c1
Christina <hristina.rozenkova@skyeng.ru>
2022-01-13 23:07:56 +0500

    Initial commit

---
+++ src/main/java/pro/sky/skyprocollectionshw/SkyproCollectionsHwApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprocollectionshw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCollectionsHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproCollectionsHwApplication.class, args);
+	}
+
+}
