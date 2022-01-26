ba485661ce943f83e8ff5f18522369830ec7cc69
Christina <hristina.rozenkova@skyeng.ru>
2022-01-26 16:56:04 +0500

    Homework for lesson 2.6

--- src/main/java/pro/sky/skyprocollectionshw/service/EmployeeService.java
+++ src/main/java/pro/sky/skyprocollectionshw/service/EmployeeService.java
@@ -1,12 +1,12 @@
 package pro.sky.skyprocollectionshw.service;
 
-import java.util.List;
+import java.util.HashMap;
 
 import pro.sky.skyprocollectionshw.data.Employee;
 
 public interface EmployeeService {
 
-    List<Employee> getAllEmployees();
+    HashMap<String, Employee> getAllEmployees();
     String addEmployee(String firstName, String lastName);
     Employee findEmployee(String firstName, String lastName);
     String removeEmployee(String firstName, String lastName);
--- src/main/java/pro/sky/skyprocollectionshw/service/impl/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/skyprocollectionshw/service/impl/EmployeeServiceImpl.java
@@ -2,8 +2,9 @@ package pro.sky.skyprocollectionshw.service.impl;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 import pro.sky.skyprocollectionshw.data.Employee;
 import pro.sky.skyprocollectionshw.exception.EmployeeAlreadyExistsException;
@@ -13,48 +14,47 @@ import pro.sky.skyprocollectionshw.service.EmployeeService;
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private final List<Employee> employees = new ArrayList<>();
+    private final Map<String, Employee> employees = new HashMap<>();
 
     @Override
-    public List<Employee> getAllEmployees() {
-        return new ArrayList<>(employees);
+    public HashMap<String, Employee> getAllEmployees() {
+        return new HashMap<>(employees);
     }
 
     @Override
     public String addEmployee(String firstName, String lastName) throws EmployeeAlreadyExistsException {
         Employee newEmployee = new Employee(firstName, lastName);
         doesEmployeeExist(newEmployee);
-        employees.add(newEmployee);
+        employees.put(UUID.randomUUID().toString(), newEmployee);
         return "Employee <b>" + firstName + " " + lastName + "</b> has been successfully added.";
     }
 
     @Override
     public Employee findEmployee(String firstName, String lastName) {
-        return employees.get(getEmployeeIndexOrError(firstName, lastName));
+        return employees.get(getEmployeeKeyOrError(firstName, lastName));
     }
 
     @Override
     public String removeEmployee(String firstName, String lastName) {
-        employees.remove(getEmployeeIndexOrError(firstName, lastName));
+        employees.remove(getEmployeeKeyOrError(firstName, lastName));
         return "Employee <b>" + firstName + " " + lastName + "</b> has been successfully fired.";
     }
 
     private void doesEmployeeExist(Employee employee) {
-        if (employees.contains(employee)) {
-            throw new EmployeeAlreadyExistsException();
+        for (Employee value: employees.values()) {
+            if (value.equals(employee)) {
+                throw new EmployeeAlreadyExistsException();
+            }
         }
     }
 
-    private int getEmployeeIndexOrError(String firstName, String lastName) throws EmployeeNotFoundException {
-        int index = getEmployeeIndex(firstName, lastName);
-        if (index >= 0) {
-            return index;
+    private String getEmployeeKeyOrError(String firstName, String lastName) throws EmployeeNotFoundException {
+        Employee employee = new Employee(firstName, lastName);
+        for (Map.Entry<String, Employee> item : employees.entrySet()) {
+            if (item.getValue().equals(employee)) {
+                return item.getKey();
+            }
         }
         throw new EmployeeNotFoundException();
-    }
-
-    private int getEmployeeIndex(String firstName, String lastName) {
-        Employee employee = new Employee(firstName, lastName);
-        return employees.indexOf(employee);
     }
 }
